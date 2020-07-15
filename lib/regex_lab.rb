aed0c17a2842be87ba77a6731f06ce3948732afb
Ian Mayton <ian.mayton@gmail.com>
2020-07-15 21:41:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,7 +3,7 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.scan(/\Aun+ing/)
+  text.scan(/un+ing/)
 end
 
 def words_five_letters_long(text)
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.scan(//)
 end
 
 def valid_phone_number?(phone)
-
+  text.scan(/[]-[]-[]/)
 end
