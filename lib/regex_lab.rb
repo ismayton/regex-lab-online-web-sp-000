af1f780464574f9779e1b4fb499f9b75486707a8
Ian Mayton <ian.mayton@gmail.com>
2020-07-15 21:46:28 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if (text.scan(/[\d{3}]-[\d{3}]-[\d{4}]/))
+  phone.scan(/[\d{3}]-[\d{3}]-[\d{4}]/)
     return true
   else
     return false
