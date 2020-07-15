e1e48871f6e48d661bf9150a734b44303d4242b5
Ian Mayton <ian.mayton@gmail.com>
2020-07-15 22:00:18 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  return word.scan(/[aeiou]/)
+  return word.scan(/\b[aeiou]/)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -15,5 +15,5 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/[\d{3}][\d{3}][\d{4}]/)
+  phone.scan(/\d{3}\d{3}\d{4}]/)
 end
