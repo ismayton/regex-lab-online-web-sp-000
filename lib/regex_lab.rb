0047978b5453e2c4ef4a26c33738dff2d15ea642
Ian Mayton <ian.mayton@gmail.com>
2020-07-15 21:46:32 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,7 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/[\d{3}]-[\d{3}]-[\d{4}]/)
+  if phone.scan(/[\d{3}]-[\d{3}]-[\d{4}]/)
     return true
   else
     return false
